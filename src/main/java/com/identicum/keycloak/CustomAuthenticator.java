6a3f6e83af6e98404845b88fff9cdf572b194820
Luciano Agustin Crocco <lucianoacrocco@gmail.com>
2025-04-01 11:07:31 -0300

    Remove old method

--- src/main/java/com/identicum/keycloak/CustomAuthenticator.java
+++ src/main/java/com/identicum/keycloak/CustomAuthenticator.java
@@ -4,9 +4,4 @@ import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
 import org.keycloak.services.messages.Messages;
 
 public class CustomAuthenticator extends UsernamePasswordForm {
-
-	@Override
-	protected String tempDisabledError() {
-		return Messages.ACCOUNT_TEMPORARILY_DISABLED;
-	}
 }
